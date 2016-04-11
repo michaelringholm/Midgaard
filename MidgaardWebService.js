dff022b0d2f58adcf0763555ace5eb80846691c0
michaelringholm <chssdm@audatex.com>
2016-04-11 14:28:48 +0200

    MidgaardMainMap seperated

--- MidgaardWebService.js
+++ MidgaardWebService.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var MapDao = require('./map/MapDao.js');
+var MidgaardMainMap = require('./map/MidgaardMainMap.js');
 var Logger = require('./common/Logger.js');
 
 var _logger = new Logger();
@@ -378,39 +379,6 @@ function MapFactory() {
 	this.construct = function() {
 		_logger.logInfo("MobFactory.construct");
 		_this.addMap(new MidgaardMainMap(_mapDao));
-	};
-	
-	_this.construct();
-}
-
-function MidgaardMainMap(mapDao) {
-	var _this = this;
-	this.key = "midgaard-main";
-	this.name = "Midgaard main map";
-	this.locations = new Array();
-	
-	this.getLocation = function(targetCoordinates) {
-		if( (targetCoordinates.x >= 0 && targetCoordinates.x <= 20) && (targetCoordinates.y >= 0 && targetCoordinates.y <= 20)  ) {
-			var possibleMobKeys = ["rat", "beetle", "spider"];
-			var mobProbability = 0.20;
-			var mob = null;
-			
-			if(Math.random() < mobProbability) {
-				//var mobIndex = Math.Round(Math.random()*possibleMobKeys.length));
-				//var mob = 
-				var mob = _mobFactory.create();
-			}
-			
-			var location = new Location({terrainType:"woodland", mob:mob});
-			return location;
-		}
-		else 
-			return null;
-	};
-	
-	this.construct = function() {
-		_logger.logInfo("MidgaardMainMap.construct");
-		mapDao.load(_this.key);
 	};
 	
 	_this.construct();
---
+++ map/MidgaardMainMap.js
@@ -0,0 +1,35 @@
+var Logger = require('../common/Logger.js');
+var _logger = new Logger();
+
+module.exports = function MidgaardMainMap(mapDao) {
+	var _this = this;
+	this.key = "midgaard-main";
+	this.name = "Midgaard main map";
+	this.locations = new Array();
+	
+	this.getLocation = function(targetCoordinates) {
+		if( (targetCoordinates.x >= 0 && targetCoordinates.x <= 20) && (targetCoordinates.y >= 0 && targetCoordinates.y <= 20)  ) {
+			var possibleMobKeys = ["rat", "beetle", "spider"];
+			var mobProbability = 0.20;
+			var mob = null;
+			
+			if(Math.random() < mobProbability) {
+				//var mobIndex = Math.Round(Math.random()*possibleMobKeys.length));
+				//var mob = 
+				var mob = _mobFactory.create();
+			}
+			
+			var location = new Location({terrainType:"woodland", mob:mob});
+			return location;
+		}
+		else 
+			return null;
+	};
+	
+	this.construct = function() {
+		_logger.logInfo("MidgaardMainMap.construct");
+		mapDao.load(_this.key);
+	};
+	
+	_this.construct();
+}
